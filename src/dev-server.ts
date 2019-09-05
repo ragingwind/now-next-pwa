bf4bdc53b94fa4ebcc7d0325cd3d3229a8c755b1
Jimmy Moon <ragingwind@gmail.com>
2019-09-05 13:32:15 +0900

    Use intersect-json

--- src/dev-server.ts
+++ src/dev-server.ts
@@ -1,27 +1,9 @@
-import resolveFrom from 'resolve-from';
-import { parse } from 'url';
-import getPort from 'get-port';
-import { createServer } from 'http';
-// import { syncEnvVars } from './utils';
 import path from 'path';
-
-interface EnvConfig {
-  [name: string]: string | undefined;
-}
-
-function syncEnvVars(base: EnvConfig, removeEnv: EnvConfig, addEnv: EnvConfig) {
-  // Remove any env vars from `removeEnv`
-  // that are not present in the `addEnv`
-  const addKeys = new Set(Object.keys(addEnv));
-  for (const name of Object.keys(removeEnv)) {
-    if (!addKeys.has(name)) {
-      delete base[name];
-    }
-  }
-
-  // Add in the keys from `addEnv`
-  Object.assign(base, addEnv);
-}
+import { parse } from 'url';
+import { createServer } from 'http';
+import getPort from 'get-port';
+import resolveFrom from 'resolve-from';
+import intersectJson from 'intersect-json';
 
 process.on('unhandledRejection', err => {
   console.error('Exiting builder due to build error:');
@@ -34,21 +16,18 @@ async function main(cwd: string) {
   const app = next({ dev: true, dir: cwd });
   const handler = app.getRequestHandler();
 
+  await app.prepare();
+
   const openPort = await getPort({
     port: [5000, 4000],
   });
-
   const url = `http://localhost:${openPort}`;
 
-  // Prepare for incoming requests
-  await app.prepare();
-
-  // The runtime env vars are passed in to `argv[2]`
-  // as a base64-encoded JSON string
   const runtimeEnv = JSON.parse(
     Buffer.from(process.argv[2], 'base64').toString()
   );
-  syncEnvVars(process.env, process.env, runtimeEnv);
+
+  process.env = intersectJson(process.env, runtimeEnv);
 
   createServer((req, res) => {
     const parsedUrl = parse(req.url || '', true);
